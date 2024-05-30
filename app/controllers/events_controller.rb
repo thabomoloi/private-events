91f351d79cfa4ee7559e87c12eadc477750a3d91
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-05-30 06:30:06 +0200

    Add controller and view for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,32 @@
 class EventsController < ApplicationController
   def index
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to events_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "events#index"
+
+  resources :users
+  resources :events
 end
