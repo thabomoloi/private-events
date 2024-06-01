0d1d88ee0e9a794c8ef31ad56a460bf39ad3d0aa
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-06-01 18:33:28 +0200

    Add tailwind to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event", dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_081200) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_044458) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.datetime "created_at", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_081200) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
